7a70ac32080a74e01c80d264c2df20b118d3ba85
don9m1n <ddmkim94@gmail.com>
2024-11-25 17:13:35 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/kr/pengu/pengu/PenguApplication.java
@@ -0,0 +1,13 @@
+package kr.pengu.pengu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenguApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/pengu/pengu/PenguApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pengu.pengu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
